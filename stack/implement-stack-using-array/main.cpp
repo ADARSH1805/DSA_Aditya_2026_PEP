5e63de5fff71d26a8c37a5e6ee3760c78c019cbe
Aditya Saxena <adity09@outlook.com>
2026-02-14 18:16:50 +0530

    Add implementations for two stack problems
    This commit introduces solutions for two common stack-related problems:
    
    - Implementing a stack using an array.
    - Reversing a stack.

---
+++ stack/implement-stack-using-array/main.cpp
@@ -0,0 +1,28 @@
+// https://www.geeksforgeeks.org/problems/implement-stack-using-array/1
+
+class myStack {
+public:
+  int cap;
+  vector<int> arr;
+
+  myStack(int n) { cap = n; }
+
+  bool isEmpty() { return arr.empty(); }
+
+  bool isFull() { return cap == arr.size(); }
+
+  void push(int x) { arr.push_back(x); }
+
+  void pop() {
+    if (!isEmpty()) {
+      arr.pop_back();
+    }
+  }
+
+  int peek() {
+    if (isEmpty())
+      return -1;
+
+    return arr.back();
+  }
+};
